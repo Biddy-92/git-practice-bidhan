bd9e51d708a7d2677b79431f0954cb514eb06259
Bidhan CKB <bidhanckb1000@gmail.com>
2026-07-01 20:30:50 +0600

    Add basic calculator functions

--- src/main.py
+++ src/main.py
@@ -5,6 +5,11 @@ def main():
     print("My name is Bidhan Chakraborty")
     print("Today's date : ", datetime.date.today())
 
+    # calculator demo
+    print("10+5 = ", add(10,5))
+    print("10-5 = ", subtract(10,5))
+    
+
 
 if __name__ == "__main__":
     main()
